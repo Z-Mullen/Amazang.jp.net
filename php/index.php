a45676d60d44ff4193de394fa2bd72d818ac7b21
[zach] <[zmullen115@gmail.com]>
2018-12-21 16:12:46 -0500

    commit

---
+++ php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Amazang.jp.net</title>
+    <meta charset="utf-8"/>
+    <meta name="keywords" content="Amazang,amazang,Amazang.jp.net,Shop"/>
+    <meta name="description" content="Amazang.jp.net the site where you can buy nothing."/>
+    <meta name="viewpoint" content="width=device-width, initial-scale=1"/>
+</head>
+
+<body>
+    
+    
+
+</body>
+</html>
